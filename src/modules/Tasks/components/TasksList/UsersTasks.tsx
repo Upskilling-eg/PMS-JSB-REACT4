80b3fcd9039e0a2f718e8608a37d4e18755269e5
Ahmed Ali <Ahmed.A.Qenawy@gmail.com>
2024-09-04 14:24:07 +0300

    adding user tasks ui

--- src/modules/Tasks/components/TasksList/UsersTasks.tsx
+++ src/modules/Tasks/components/TasksList/UsersTasks.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./UsersTasks.css";
+import { range } from "../../../../hooks/usePaginate";
 
 const UsersTasks = () => {
   return (
@@ -10,6 +11,27 @@ const UsersTasks = () => {
       <div className="tasks-board">
         <div className="column">
           <h3>To Do</h3>
+          <div className="cards">
+            {range({ start: 1, end: 5 }).map((index) => (
+              <div className="card">card description</div>
+            ))}
+          </div>
+        </div>
+        <div className="column">
+          <h3>In Progress</h3>
+          <div className="cards">
+            {range({ start: 1, end: 5 }).map((index) => (
+              <div className="card">card description</div>
+            ))}
+          </div>
+        </div>
+        <div className="column">
+          <h3>Done</h3>
+          <div className="cards">
+            {range({ start: 1, end: 5 }).map((index) => (
+              <div className="card">card description</div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
